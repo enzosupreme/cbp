9b0036efb2fb1f4e23c8ef436d4a5b5c41738ae3
My <myemail@example.com>
2023-10-21 18:32:33 -0500

    prices added

--- projects/models.py
+++ projects/models.py
@@ -38,6 +38,7 @@ class Project(models.Model):
     github = models.URLField(max_length=200, blank=True, null=True)
     youtube = models.URLField(max_length=200, blank=True, null=True)
     shop = models.URLField(max_length=200, blank=True, null=True)
+    price = models.DecimalField(max_digits=6,decimal_places=2,blank=True,null=True)
 
     def __str__(self):
         return self.name
