6fc766b01a6783cda15a8695a8b1460564785159
My <myemail@example.com>
2024-09-15 15:24:44 -0500

    terrarium stuff

--- terrarium/models.py
+++ terrarium/models.py
@@ -4,7 +4,7 @@ class Plant(models.Model):
     name = models.CharField(max_length=2555)
     description = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to='plant_images', blank=True, null=True)
-    pictures = models.URLField(max_length=200, blank=True, null=True)
+    link = models.URLField(max_length=200, blank=True, null=True)
 
 
     def __str__(self):
